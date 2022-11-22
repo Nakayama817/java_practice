15ad39e2189161018675633e0906175ae614fb1d
Nakayama817 <keita.salinger.8316@gmail.com>
2022-11-23 03:07:02 +0900

    complite 11/23

---
+++ SeismicMagnitudeScales.java
@@ -0,0 +1,23 @@
+//complite 11/23
+
+import java.util.stream.Stream;
+
+public class SeismicMagnitudeScales {
+
+	public static void main(String args[]) {
+
+        String[] stringArray = new java.util.Scanner(System.in).nextLine().split(" ");
+
+        //int[] intArray = new int[0];
+        //for(int i = 0; i < stringArray.length; i++){
+        //    intArray[i] = Integer.parseInt(stringArray[i]);
+        //}
+
+        int[] intArray = Stream.of(stringArray).mapToInt(Integer::parseInt).toArray();
+
+
+		System.out.println((int)Math.pow(32, intArray[0] - intArray[1]));
+
+	}
+
+}
