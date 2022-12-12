f929535f1291162c2f49475e0fea9533ae290f6f
Nakayama817 <keita.salinger.8316@gmail.com>
2022-12-12 14:16:21 +0900

    12/12

---
+++ hatake.java
@@ -0,0 +1,18 @@
+//12/12 complite
+
+import java.util.*;
+public class Main {
+	public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        String[] sAry = scanner.nextLine().split(" ");      //difference nextLine(), next()?
+        
+        scanner.close();
+        
+        int x = Integer.parseInt(sAry[0]);
+        int y = Integer.parseInt(sAry[1]);
+        
+        System.out.println((x - 2) * (y - 2));
+        
+    }
+    
+}
